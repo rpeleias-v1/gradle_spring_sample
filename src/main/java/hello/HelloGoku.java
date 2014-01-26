8436d06c890361a8abf0314116e576e448794ffd
Rodrigo Peleias <=>
2014-01-26 21:07:48 -0200

    Add gradle wrapper

--- src/main/java/hello/HelloGoku.java
+++ src/main/java/hello/HelloGoku.java
@@ -1,8 +1,13 @@
 package hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloGoku {
   
   public static void main(String args[]) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("Current local time: " + currentTime);
+
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
